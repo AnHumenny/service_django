130cee5e6d5f174e15a0c243ebc13e09b6e2ff9d
Andrei Humenniy <an.gumenniy@gmail.com>
2024-12-08 08:17:29 +0300

    исправлен url TinyMCE

--- base/settings.py
+++ base/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
+from tinymce.widgets import TinyMCE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,5 +150,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-TINYMCE_JS_URL = 'https://cdn.tiny.cloud/1/3r1qfnyory13br9dbawy1fqparoiltfh8lxl9z2kbsf9c8xn/tinymce/5/tinymce.min.js'
+TINYMCE_JS_URL = os.getenv("TINYMCE_URL")
 TINYMCE_COMPRESSOR = False
