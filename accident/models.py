a2c571390380a4e87a5b83e37b8bdae540ca0a03
Andrei Humenniy <an.gumenniy@gmail.com>
2024-12-10 09:53:51 +0300

    Изменена модель, добавлены выпадающие поля статуса, добавлена проверка на уникальность номера заявки

--- accident/models.py
+++ accident/models.py
@@ -2,27 +2,33 @@
 from django.db import models
 from django.utils import timezone
 from tinymce.models import HTMLField
+from django.utils.translation import gettext_lazy as _
 
 
 class Accident(
-    models.Model):  # Создаём новый класс, который будет служить для блога моделью, указывая все необходимые элементы.
-    number = models.CharField(max_length=10)
-    category = models.CharField(max_length=20)
-    sla = models.CharField(max_length=20)
+    models.Model):
+    number = models.CharField(max_length=10, unique=True)
+    category = models.CharField(max_length=20, blank=True)
+    sla = models.CharField(max_length=20, blank=True, null=True)
     datetime_open = models.DateTimeField(default=timezone.now)
-    datetime_close = models.DateTimeField(default=timezone.now)
+    datetime_close = models.DateTimeField(default=timezone.now, blank=True, null=True)
     problem = HTMLField()
     city = models.CharField(max_length=255)
     address = models.CharField(max_length=500)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=13)
     subscriber = models.CharField(max_length=13)
-    comment = HTMLField()
-    decide = HTMLField()
-    status = models.CharField(max_length=5)
+    comment = HTMLField(blank=True)
+    decide = HTMLField(blank=True)
+    STATUS_CHOICES = (
+        ('open', _('Open')),
+        ('check', _('Check')),
+        ('close', _('Close')),
+    )
+    status = models.CharField(max_length=5, choices=STATUS_CHOICES, default='open')
 
-    def __str__(self):  # С помощью функции меняем то, как будет представлена запись в модели.
-        return f"Инцидент {self.number}, {self.city}, {self.address}"  # Указываем, что она будет идентифицироваться с помощью своего заголовка.
+    def __str__(self):
+        return f"Инцидент {self.number}, {self.city}, {self.address}"
 
     class Meta:
         verbose_name_plural = "Accidents"
