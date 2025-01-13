65b3075fc240d6186b06002775b578222aed6fe0
Andrei Humenniy <an.gumenniy@gmail.com>
2025-01-13 22:30:52 +0300

    добавлены модели Material, ExpansionFttx

--- index/models.py
+++ index/models.py
@@ -14,3 +14,35 @@ class Index(
 
     class Meta:
         verbose_name_plural = "Index_x"  # Указываем правильное написание для множественного числа слова Entry
+
+
+class Material(models.Model):
+    date = models.DateField(verbose_name="Дата")
+    cable = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.date} - {self.cable}"
+
+    class Meta:
+        db_table = "_material"
+        verbose_name = "Материал"
+        verbose_name_plural = "Материалы"
+
+
+class ExpansionFttx(models.Model):
+    date = models.DateField(verbose_name="Дата")
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    cable = models.IntegerField()
+    connector = models.IntegerField()
+    crossbox = models.IntegerField()
+    plint = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.date} - {self.address}"
+
+    class Meta:
+        db_table = "_expansion"
+        verbose_name = "Дата"
+        verbose_name_plural = "Адрес"
